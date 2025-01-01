6ee5ff6c4634a5ac7e05ab09194ad00be4d6981c
serhiivolynets-256 <sergvoly-ipt24@lll.kpi.ua>
2025-01-01 17:13:30 +0200

    append sign and verify functions

--- src/main.py
+++ src/main.py
@@ -3,25 +3,42 @@ from sympy.abc import x
 from sympy import div
 from sympy import symbols, GF, div, Poly
 import random
+import hashlib
+import math
 
 N = 256
 Q = 2**23 - 2**13 + 1 
 GAMMA1 = (Q - 1) // 16
 GAMMA2 = GAMMA1 // 2
-BETA = 375
-ALPHA = 2 * GAMMA2
-
 K = 3
 L = 2
-ETA = 7
-SETABITS = 4
-BETA = 375
-OMEGA = 64
+BETA = 175
+ETA = 3
 
-# x = symbols('X')
+ALPHA = 2 * GAMMA2
+
 Fq = GF(Q)
 
 MODULUS = x**N + 1
+
+SHAKE128_LENGTH = (256 // 4)
+# SHAKE128_LENGTH = 4
+
+def SHAKE128(s: str) -> list[int]:
+    # return int(hashlib.shake_128(s.encode('utf-8')).hexdigest(SHAKE128_LENGTH), base=16)
+    number = int(hashlib.shake_128(s.encode('utf-8')).hexdigest(SHAKE128_LENGTH), base=16)
+
+    # if number.bit_length != 256:
+        # raise ValueError("wrong size")
+    
+    c = [0 for i in range(256)]
+
+    for i in range(60):
+        c[i] = pow(-1, number & 1)
+        number = number >> 1
+    
+    return Poly(c, x, domain=Fq)
+
 
 def random_polynomial_Zq(use_eta: bool=False):
     # Create the polynomial with random coefficients in Z_q
@@ -53,6 +70,17 @@ def matrix_mul(A :list[list], B :list[list]):
     
     return C
 
+def matrix_const_mul(A :list[list], c :int):
+    n = len(A)
+    m = len(A[0])
+
+    C = [[None for j in range(m)] for i in range(n)]
+    for i in range(n):
+        for j in range(m):
+            C[i][j] = div(c * A[i][j], MODULUS)[1]
+    
+    return C
+
 def matrix_sum(A :list[list], B :list[list]):
     n = len(A)
     m = len(A[0])
@@ -66,26 +94,49 @@ def matrix_sum(A :list[list], B :list[list]):
     
     return C
 
+def matrix_sub(A :list[list], B :list[list]):
+    n = len(A)
+    m = len(A[0])
+    if len(B) != n or len(B[0]) != m:
+        raise ValueError(f"Inappropriate matrices:: (nxm) = {n}x{m}, (nxm) = {len(B)}x{len(B[0])}")
+
+    C = [[None for j in range(m)] for i in range(n)]
+    for i in range(n):
+        for j in range(m):
+            C[i][j] = div(A[i][j] - B[i][j], MODULUS)[1]
+    
+    return C
+
+# def decompose(a):
+#     # Centralized remainder mod ALPHA
+#     t = a & 0x7FFFF
+#     t += (a >> 19) << 9
+#     t -= ALPHA // 2 + 1
+#     t += (t >> 31) & ALPHA
+#     t -= ALPHA // 2 - 1
+#     a -= t
+
+#     # Divide by ALPHA (possible to avoid)
+#     u = a - 1
+#     u >>= 31
+#     a = (a >> 19) + 1
+#     a -= u & 1
+
+#     # Border case
+#     a0 = Q + t - (a >> 4)
+#     a &= 0xF
+#     return a, a0
 
 def decompose(a):
-    # Centralized remainder mod ALPHA
-    t = a & 0x7FFFF
-    t += (a >> 19) << 9
-    t -= ALPHA // 2 + 1
-    t += (t >> 31) & ALPHA
-    t -= ALPHA // 2 - 1
-    a -= t
+    r = a % Q
+    r0 = modpm(r, modulus=ALPHA)
+    if r - r0 == Q - 1:
+        r1 = 0
+        r0 = r0 - 1
+    else:
+        r1 = (r - r0) // ALPHA
 
-    # Divide by ALPHA (possible to avoid)
-    u = a - 1
-    u >>= 31
-    a = (a >> 19) + 1
-    a -= u & 1
-
-    # Border case
-    a0 = Q + t - (a >> 4)
-    a &= 0xF
-    return a, a0
+    return (r1, r0)
 
 
 def Gen():
@@ -98,13 +149,87 @@ def Gen():
 
     return ((A, t), (A, t, s1, s2))
 
+def HighBits(a):
+    r1, r0 = decompose(a)
+    return r1
+
+def LowBits(a):
+    r1, r0 = decompose(a)
+    return r0
+
+def modpm(a, modulus: int=Q):
+    res = a % modulus
+    if res > (modulus // 2):
+        res = abs(res - modulus)
+    
+    return res
+
+def Norm_polyvec(polyveck):
+    res = -math.inf;
+
+    for poly in polyveck:
+        for coeff in poly[0].all_coeffs():
+            res = max(res, modpm(coeff))
+
+    return res;
+
+def Norm_polyvec_check(polyveck, b):
+    for poly in polyveck:
+        for coeff in poly[0].all_coeffs():
+            if modpm(coeff) >= b:
+                return True
+
+    return False;
+
+def Norm_matr(matr):
+    res = -math.inf;
+
+    for row in matr:
+        for cell in row:
+            res = max(res, modpm(cell))
+
+    return res;
+
+def Norm_matr_check(matr, b):
+    for row in matr:
+        for cell in row:
+            if modpm(cell) >= b:
+                return True
+
+    return False
+
 def Sign(sk, M):
-    pass
+    A, t, s1, s2 = sk
 
-def Verify():
-    pass
+    z = None
 
-def example():
+    while z is None:
+        y = [[random_polynomial_Zq(use_eta=True)] for i in range(L)]
+        w1 = [[HighBits(coef) for coef in poly[0].all_coeffs()] for poly in matrix_mul(A, y)]
+        c = SHAKE128(M + str(w1))
+        z = matrix_sum(y, matrix_const_mul(c=c, A=s1))
+        temp_value = matrix_sub(matrix_mul(A, y), matrix_const_mul(s2, c=c))
+        temp_value = [[LowBits(coef) for coef in poly[0].all_coeffs()] for poly in temp_value]
+        
+        ch1 = Norm_polyvec_check(z, b=(GAMMA1 - BETA))
+        ch2 = Norm_matr_check(temp_value, b=(GAMMA2 - BETA))
+        if ch1 or ch2:          
+            z = None
+            print("...next iteration...")
+
+    return (z, c)
+
+def Verify(pk, M, sigma):
+    A, t = pk
+    z, c = sigma
+
+    temp_value = matrix_sub(matrix_mul(A, z), matrix_const_mul(t, c=c))
+    w1 = [[HighBits(coef) for coef in poly[0].all_coeffs()] for poly in temp_value]
+
+    return Norm_polyvec(z) < (GAMMA1 - BETA) and c == SHAKE128(M + str(w1))
+
+
+def example(): # polinomial multipication modulo MODULUS (which is polinom)
     polynomial1 = random_polynomial_Zq()
     polynomial2 = random_polynomial_Zq()
 
@@ -119,11 +244,31 @@ def example():
     print(f"Multiplication result modulo {MODULUS}: {result_mul}")
 
 def main():
+    print("Key generation...")
     pk, sk = Gen()
-    print(pk)
-    print("------------------------------------------")
-    print(sk)
+    print("Key is generated")
+    print("")
+
+
+    M1 = "aaaaaaaaaaaaa"
+    M2 = "aaaaaaaaaaaaaaa"
+    print("Sign first message...")
+    sigma1 = Sign(sk=sk, M=M1)
+    print("Sign second message...")
+    sigma2 = Sign(sk=sk, M=M2)
+    print("Messages are signed...")
+    print("")
+
+    print("Verify signature of the messages")
+    print(f"First signature correctness:", Verify(pk=pk, M=M1, sigma=sigma1))
+    print(f"Seccond signature correctness:", Verify(pk=pk, M=M2, sigma=sigma2))
+    print("")
+
+    print("Sanity check...")
+    print("Check Verify(pk, M1, sigma2):", not Verify(pk=pk, M=M1, sigma=sigma2))
+    print("Check Verify(pk, M2, sigma1):", not Verify(pk=pk, M=M2, sigma=sigma1))
+
 
 if __name__ == "__main__":
     main()
-    # example()
+
